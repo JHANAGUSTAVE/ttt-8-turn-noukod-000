61bec8effbe49bb222b7025e9a73a93c38e4a5e5
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-09 20:41:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,18 @@
 def display_board(board)
+<<<<<<< HEAD
 print " #{board[0]} | #{board[1]} | #{board[2]} "
 print "-----------"
 print " #{board[3]} | #{board[4]} | #{board[5]} "
 print "-----------"
 print " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+print " X | X | X "
+print "-----------"
+print " X | O | O "
+print " X | O | X "
+print " O | X | X "
+print " O | X | O "
+>>>>>>> 6598a65b8bd0e7f25befbd86ed0ee3d7c6037f5c
 end
 
 user_input ="1"
@@ -31,7 +40,11 @@ end
 
 
 def move(board, index, value ="X")
+<<<<<<< HEAD
  board[index] = value
+=======
+ board[index] == " "
+>>>>>>> 6598a65b8bd0e7f25befbd86ed0ee3d7c6037f5c
 end
 
 
@@ -39,6 +52,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
+<<<<<<< HEAD
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
@@ -46,4 +60,9 @@ def turn(board)
   else 
    turn(board)
   end
+=======
+ input_to_index(input)
+ 
+  
+>>>>>>> 6598a65b8bd0e7f25befbd86ed0ee3d7c6037f5c
 end
